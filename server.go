5e50bd92bd86cd5390b371afe820715fdcd5720b
devin_yee <smartlyfly@gmail.com>
2015-05-26 16:14:57 +0800

    using controllers

--- server.go
+++ server.go
@@ -1,45 +1,23 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 
-	"github.com/devin0910/go-rest/models"
+	"github.com/devin0910/go-rest/controllers"
 	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
 	r := httprouter.New()
 
-	r.GET("/user/:id", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-		u := models.User{
-			Name:   "Bob Smith",
-			Gender: "male",
-			Age:    50,
-			Id:     p.ByName("id"),
-		}
+	uc := controllers.NewUserController()
 
-		uj, _ := json.Marshal(u)
+	r.GET("/user/:id", uc.GetUser)
 
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(200)
-		fmt.Fprintf(w, "%s", uj)
-	})
+	r.POST("/user", uc.CreateUser)
 
-	r.POST("/user", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-		u := models.User{}
-
-		json.NewDecoder(r.Body).Decode(&u)
-
-		u.Id = "foo"
-
-		uj, _ := json.Marshal(u)
-
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(201)
-		fmt.Fprintf(w, "%s", uj)
-	})
+	r.DELETE("/user/:id", uc.RemoveUser)
 
 	r.GET("/test", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 		fmt.Fprint(w, "Welcome!\n")
